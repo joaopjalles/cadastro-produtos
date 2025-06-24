f2ace865039427191d08247ceb0bfda30a4417a7
joaopjalles <jpedrojalles@gmail.com>
2025-06-24 18:27:28 -0300

    Corrige configuração da API e endpoints

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,29 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: 'https://apipw.leoproti.com.br'
+  baseURL: 'https://apipw.leoproti.com.br',
+  timeout: 5000,
+  headers: {
+    'Content-Type': 'application/json',
+    'Accept': 'application/json'
+  }
+});
+
+// Interceptor para logging
+api.interceptors.request.use(config => {
+  console.log('Enviando requisição:', config);
+  return config;
+}, error => {
+  console.error('Erro na requisição:', error);
+  return Promise.reject(error);
+});
+
+api.interceptors.response.use(response => {
+  console.log('Resposta recebida:', response);
+  return response;
+}, error => {
+  console.error('Erro na resposta:', error.response?.data || error.message);
+  return Promise.reject(error);
 });
 
 export default api;
