ec32b4c084f8118ef424f74ce1a9453ad973d097
joaopjalles <jpedrojalles@gmail.com>
2025-06-24 18:29:13 -0300

    Corrige configuração da API e endpoints

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'https://apipw.leoproti.com.br',
+  baseURL: 'https://apipw.leoproti.com.br/produtos',
   timeout: 5000,
   headers: {
     'Content-Type': 'application/json',
